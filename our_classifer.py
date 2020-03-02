53b7d05e88f3671f63f6c04e5fd2eab0fa643779
ddthuan <59833376+ddthuan@users.noreply.github.com>
2020-03-02 15:54:10 +0800

    Update our_classifer.py

--- our_classifer.py
+++ our_classifer.py
@@ -3,25 +3,6 @@
 import torch
 from torch import nn
 from collections import OrderedDict
-
-class OurLinear(nn.Module):
-    def __init__(self, in_features, out_features, bias=True):
-        super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.bias = bias
-        self.weight = torch.nn.Parameter(torch.randn(out_features, in_features))
-        self.bias = torch.nn.Parameter(torch.randn(out_features))       
-        
-    def forward(self, input):
-        x, y = input.shape
-# =============================================================================
-#         if y != self.in_features:
-#             sys.exit(f'Wrong Input Features. Please use tensor with {self.in_features} Input Features')
-# =============================================================================
-        output = input @ self.weight.t() + self.bias
-        return output    
-
 import math
 
 class OurLinear_I1(nn.Module):
